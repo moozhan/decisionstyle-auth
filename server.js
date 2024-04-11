2566937381019aa60c9dac56da06582c4824a52a
moozhan <moozhansh@gmail.com>
2024-04-11 18:16:39 +0100

    links fixed

--- server.js
+++ server.js
@@ -8,7 +8,6 @@ const bodyParser = require('body-parser');
 const passport = require('passport');
 const path = require('path');
 const cors = require('cors');
-const session = require('express-session');
 const flash = require('express-flash');
 
 const authRoutes = require('./routes/authRoutes');
@@ -17,11 +16,6 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 app.use(express.static(path.join(__dirname, './public')));
 app.use(flash());
-app.use(session({
-  secret: process.env.SESSION_SECRET,
-  resave: false,
-  saveUninitialized: false
-}))
 
 app.use(cors({
   origin: function (origin, callback) {
