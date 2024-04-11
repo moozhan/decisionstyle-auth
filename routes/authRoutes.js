ca1eca7a5575b6bf0b9de80b84561858b4aa0596
moozhan <moozhansh@gmail.com>
2024-04-11 16:28:31 +0100

    links fixed

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -21,7 +21,7 @@ router.use(cors({
 }));
 
 // List of allowed origins
-const allowedOrigins = ['http://localhost:5500', 'http://localhost:3000', 'https://decisionauthserver-92e41a504ad4.herokuapp.com','http://localhost:3001'];
+const allowedOrigins = ['http://localhost:5500', 'https://decisionauthserver-92e41a504ad4.herokuapp.com', 'https://decisionserver-51961461dcec.herokuapp.com', 'http://localhost:3000', 'http://localhost:3001'];
 
 // Dynamic CORS policy
 const dynamicCors = (req, callback) => {
