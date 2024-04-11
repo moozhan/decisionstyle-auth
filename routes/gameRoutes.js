9b1a932ad392068aec380d9172c3b6c292ba9fe6
moozhan <moozhansh@gmail.com>
2024-04-11 18:39:58 +0100

    links fixed

--- routes/gameRoutes.js
+++ routes/gameRoutes.js
@@ -9,32 +9,13 @@ const games = [
   // Add more games as needed
 ];
 
-// Middleware to verify the JWT token
-const verifyToken = (req, res, next) => {
-  const bearerHeader = req.headers['authorization'];
-  if (typeof bearerHeader !== 'undefined') {
-    const bearerToken = bearerHeader.split(' ')[1];
-    req.token = bearerToken;
-    jwt.verify(req.token, process.env.SECRET_KEY, (err, authData) => {
-      if (err) {
-        res.sendStatus(403); // Forbidden if token is invalid
-      } else {
-        req.authData = authData; // Forward the authentication data
-        next();
-      }
-    });
-  } else {
-    res.sendStatus(403); // Forbidden if token is not provided
-  }
-};
-
 // Route to list all available games
-router.get('/list', verifyToken, (req, res) => {
+router.get('/list', (req, res) => {
   res.json(games);
 });
 
 // Example route to access a specific game by id
-router.get('/:id', verifyToken, (req, res) => {
+router.get('/:id', (req, res) => {
   const gameId = parseInt(req.params.id);
   const game = games.find(g => g.id === gameId);
   if (game) {
