6f82614daeae3e14e70ee0970cde4dd9058eaf05
moozhan <moozhansh@gmail.com>
2024-04-11 17:23:46 +0100

    links fixed

--- routes/validateCsrfAndJwt.js
+++ routes/validateCsrfAndJwt.js
@@ -1,23 +1,24 @@
 const jwt = require('jsonwebtoken');
 
 const validateCsrfAndJwt = (req, res, next) => {
-    const tokenFromHeader = req.headers['x-csrf-token'];
     const csrfTokenFromCookie = req.cookies['XSRF-TOKEN'];
+    const tokenFromHeader = req.headers['x-csrf-token'];
     const authToken = req.cookies['AuthToken'];
-    const csrfTokenFromHeader = req.headers['x-csrf-token'];
-    console.log('CSRF Token from Header:', csrfTokenFromHeader);
+
+    console.log('CSRF Token from Header:', tokenFromHeader);
+    console.log('CSRF Token from Cookie:', csrfTokenFromCookie);
 
     if (!csrfTokenFromCookie || !tokenFromHeader || csrfTokenFromCookie !== tokenFromHeader) {
-        return res.status(403).send({ message: "Invalid CSRF token." });
+        return res.status(403).json({ message: "Invalid CSRF token." });
     }
 
     if (!authToken) {
-        return res.status(401).send({ message: "No authentication token provided." });
+        return res.status(401).json({ message: "No authentication token provided." });
     }
 
     jwt.verify(authToken, process.env.SECRET_KEY, (err, user) => {
         if (err) {
-            return res.status(403).send({ message: "Invalid authentication token." });
+            return res.status(403).json({ message: "Invalid authentication token." });
         }
 
         req.user = user; // Attach the user payload to the request object
